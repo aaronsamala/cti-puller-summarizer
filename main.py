1df98d1845a04add5f39e47570b82be61fd1c70e
aaronsamala <aaronsamala@gmail.com>
2024-02-21 23:40:24 -1000

    Add main.py as the entry point for the application

---
+++ main.py
@@ -0,0 +1,5 @@
+# this will be the main python application
+# it will be the entry point for the application
+# it will be the main file that will be run
+# it will be the file that will be run by the user
+# it will call the pull-feed.py, it will provide the feedName as the first argument and the feedURL as the second argument
